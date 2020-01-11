3126987158e631faadb5764f38757f0009311bb4
Jamail <j.lavan@hotmail.com>
2020-01-11 11:23:12 -0800

    Mustache Start

---
+++ digitalcrafts/projects/scheduler/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const mustacheExpress = require('mustache-express');
+const engine = mustacheExpress();
+ 
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress());
+ 
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+
+    res.render('index.mustache');
+
+})
+
+app.listen(3000, function(){
+    console.log("Server is runniing on 3000")
+})
+
+
+
+
+
