14756e8c362d36c1e3df8d02e42d36f7e7f0a56f
Jamail <j.lavan@hotmail.com>
2020-01-12 15:29:39 -0800

    login register modal

--- digitalcrafts/projects/scheduler/app.js
+++ digitalcrafts/projects/scheduler/app.js
@@ -1,9 +1,118 @@
 const express = require("express");
 const app = express();
-
+/* const models = require('./models'); */
+/* const session = require("express-session"); */
 const bodyParser = require("body-parser");
 const mustacheExpress = require('mustache-express');
 const engine = mustacheExpress();
+/* require('dotenv').config(); 
+
+var pbkdf2 = require('pbkdf2'); 
+var salt = process.env.SALT_KEY;
+
+function encryptionPassword(password) {
+  var key = pbkdf2.pbkdf2Sync(
+    password, salt, 36000, 256, 'sha256'
+  );
+  var hash = key.toString('hex');
+
+  return hash;
+}
+
+app.use(session({
+  secret: "cats", 
+  resave: false, 
+  saveUninitialized: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + '/public')); */
+
+/*  PASSPORT SETUP  */
+
+/* const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', function(req, res) {
+  if(req.isAuthenticated()) {
+    res.send("Welcome " + req.user.username + "!!");
+  } else {
+    res.send("not authorized.");
+  }
+});
+
+app.get('/logout', function(req, res) {
+  if(req.isAuthenticated()){
+    console.log("user logging out");
+    req.logOut();
+    res.send("user has logged out");
+  } else {
+    res.send("You don't have a session open");
+  }
+});
+
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+  models.user.findOne({ where: { id: id } }).then(function (user) {
+    cb(null, user);
+  });
+});   */
+
+/* PASSPORT LOCAL AUTHENTICATION */
+
+/* const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    models.user.findOne({
+      where: {
+        username: username
+      }
+    }).then(function (user) {
+      if (!user) {
+        return done(null, false);
+      }
+
+      if (user.password != encryptionPassword(password)) {
+        return done(null, false);
+      }
+      return done(null, user);
+    }).catch(function (err) {
+      return done(err);
+    });
+  }
+));
+
+app.post('/',
+  passport.authenticate('local', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/success');
+  });
+
+
+app.post("/sign-up", function (req, response) {
+  models.user.create({ 
+    username: req.body.username, 
+    password: encryptionPassword(req.body.password)
+  })
+    .then(function (user) {
+      response.send(user);
+    });
+    
+});
+
+app.listen(process.env.PORT, function () {
+  console.log('server listening on port ' + 
+  process.env.PORT + ' app name = ' + 
+  process.env.PROJECT_NAME);
+}) */
  
 // Register '.mustache' extension with The Mustache Express
 app.engine('mustache', mustacheExpress());
